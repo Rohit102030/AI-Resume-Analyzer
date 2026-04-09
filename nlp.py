866a1cefa953d42ce0f3c0adf20f165c04c1a8a4
Rohit Kumar <154722581+Rohit102030@users.noreply.github.com>
2026-04-09 12:31:43 +0530

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,6 +1,11 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def clean_text(text):
     doc = nlp(text)
